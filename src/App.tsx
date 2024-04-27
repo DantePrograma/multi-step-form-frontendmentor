7c23415fb7ba945fd2d5147e736e7242c06aa239
DantePrograma <136545129+DantePrograma@users.noreply.github.com>
2024-04-27 10:18:49 -0300

    fixed error in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ import { StepTwo } from "./components/StepTwo";
 import { StepThree } from "./components/StepThree";
 import { StepFour } from "./components/StepFour";
 import { Finish } from "./components/Finish";
-import sideBarDesktop from "../assets/images/bg-sidebar-desktop.svg";
-import sideBarMobile from "../assets/images/bg-sidebar-mobile.svg";
+import { NavBar } from "./components/NavBar";
 
 const steps = [
   {
@@ -97,92 +96,7 @@ function App() {
     <FormProvider {...methods}>
       <div className="w-full h-screen grid place-items-center">
         <div className="w-full h-screen md:max-w-[1000px] md:p-3 rounded md:bg-white md:h-[700px] md:grid md:grid-cols-[270px_auto] lg:grid-cols-[320px_auto] md:place-items-center md:rounded-lg overflow-hidden">
-          <nav
-            style={{
-              backgroundImage: `url(${
-                windowWidth > 768 ? sideBarDesktop : sideBarMobile
-              })`,
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-            }}
-            className="h-[16rem] md:h-full absolute md:relative p-12 md:p-7 md:pt-9 w-full flex md:flex-col font-medium gap-8 justify-center md:justify-start md:rounded-lg"
-          >
-            <div className="md:flex gap-3 items-center">
-              <button
-                style={{
-                  backgroundColor: currentStep === 0 ? "#bfe2fd" : "",
-                  color: currentStep === 0 ? "#02295a" : "#bfe2fd",
-                  border: "1px solid #bfe2fd",
-                }}
-                className="text-[#bfe2fd] rounded-full h-10 w-10 transition"
-              >
-                1
-              </button>
-              {windowWidth > 768 && (
-                <div>
-                  <p className="text-[#d6d9e6]">STEP 1</p>
-                  <h1 className="text-white font-bold">YOUR INFO</h1>
-                </div>
-              )}
-            </div>
-            <div className="md:flex gap-3 items-center">
-              <button
-                style={{
-                  backgroundColor: currentStep === 1 ? "#bfe2fd" : "",
-                  color: currentStep === 1 ? "#02295a" : "#bfe2fd",
-                  border: "1px solid #bfe2fd",
-                }}
-                className="text-[#bfe2fd] rounded-full h-10 w-10 transition"
-              >
-                2
-              </button>
-              {windowWidth > 768 && (
-                <div>
-                  <p className="text-[#d6d9e6]">STEP 2</p>
-                  <h1 className="text-white font-bold">SELECT PLAN</h1>
-                </div>
-              )}
-            </div>
-
-            <div className="md:flex gap-3 items-center">
-              <button
-                style={{
-                  backgroundColor: currentStep === 2 ? "#bfe2fd" : "",
-                  color: currentStep === 2 ? "#02295a" : "#bfe2fd",
-                  border: "1px solid #bfe2fd",
-                }}
-                className="text-[#bfe2fd] rounded-full h-10 w-10 transition"
-              >
-                3
-              </button>
-              {windowWidth > 768 && (
-                <div>
-                  <p className="text-[#d6d9e6]">STEP 3</p>
-                  <h1 className="text-white font-bold">ADD-ONS</h1>
-                </div>
-              )}
-            </div>
-
-            <div className="md:flex gap-3 items-center">
-              <button
-                style={{
-                  backgroundColor: currentStep === 3 ? "#bfe2fd" : "",
-                  color: currentStep === 3 ? "#02295a" : "#bfe2fd",
-                  border: "1px solid #bfe2fd",
-                }}
-                className="text-[#bfe2fd] rounded-full h-10 w-10 transition"
-              >
-                4
-              </button>
-              {windowWidth > 768 && (
-                <div>
-                  <p className="text-[#d6d9e6]">STEP 4</p>
-                  <h1 className="text-white font-bold">SUMMARY</h1>
-                </div>
-              )}
-            </div>
-          </nav>
+          <NavBar windowWidth={windowWidth} currentStep={currentStep} />
           <div className="w-full md:h-full md:bg-white">
             <form
               className="w-full md:h-full px-2 md:px-0 max-w-xl md:pb-3 md:pt-6 rounded-lg relative md:static md:flex md:justify-between md:flex-col top-32 mx-auto"
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,99 @@
+import sideBarDesktop from "../../assets/images/bg-sidebar-desktop.svg";
+import sideBarMobile from "../../assets/images/bg-sidebar-mobile.svg";
+
+export const NavBar = ({
+  windowWidth,
+  currentStep,
+}: {
+  windowWidth: number;
+  currentStep: number;
+}) => {
+  return (
+    <nav
+      style={{
+        backgroundImage: `url(${
+          windowWidth > 768 ? sideBarDesktop : sideBarMobile
+        })`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+      className="h-[16rem] md:h-full absolute md:relative p-12 md:p-7 md:pt-9 w-full flex md:flex-col font-medium gap-8 justify-center md:justify-start md:rounded-lg"
+    >
+      <div className="md:flex gap-3 items-center">
+        <button
+          style={{
+            backgroundColor: currentStep === 0 ? "#bfe2fd" : "",
+            color: currentStep === 0 ? "#02295a" : "#bfe2fd",
+            border: "1px solid #bfe2fd",
+          }}
+          className="text-[#bfe2fd] rounded-full h-10 w-10 transition"
+        >
+          1
+        </button>
+        {windowWidth > 768 && (
+          <div>
+            <p className="text-[#d6d9e6]">STEP 1</p>
+            <h1 className="text-white font-bold">YOUR INFO</h1>
+          </div>
+        )}
+      </div>
+      <div className="md:flex gap-3 items-center">
+        <button
+          style={{
+            backgroundColor: currentStep === 1 ? "#bfe2fd" : "",
+            color: currentStep === 1 ? "#02295a" : "#bfe2fd",
+            border: "1px solid #bfe2fd",
+          }}
+          className="text-[#bfe2fd] rounded-full h-10 w-10 transition"
+        >
+          2
+        </button>
+        {windowWidth > 768 && (
+          <div>
+            <p className="text-[#d6d9e6]">STEP 2</p>
+            <h1 className="text-white font-bold">SELECT PLAN</h1>
+          </div>
+        )}
+      </div>
+
+      <div className="md:flex gap-3 items-center">
+        <button
+          style={{
+            backgroundColor: currentStep === 2 ? "#bfe2fd" : "",
+            color: currentStep === 2 ? "#02295a" : "#bfe2fd",
+            border: "1px solid #bfe2fd",
+          }}
+          className="text-[#bfe2fd] rounded-full h-10 w-10 transition"
+        >
+          3
+        </button>
+        {windowWidth > 768 && (
+          <div>
+            <p className="text-[#d6d9e6]">STEP 3</p>
+            <h1 className="text-white font-bold">ADD-ONS</h1>
+          </div>
+        )}
+      </div>
+
+      <div className="md:flex gap-3 items-center">
+        <button
+          style={{
+            backgroundColor: currentStep === 3 ? "#bfe2fd" : "",
+            color: currentStep === 3 ? "#02295a" : "#bfe2fd",
+            border: "1px solid #bfe2fd",
+          }}
+          className="text-[#bfe2fd] rounded-full h-10 w-10 transition"
+        >
+          4
+        </button>
+        {windowWidth > 768 && (
+          <div>
+            <p className="text-[#d6d9e6]">STEP 4</p>
+            <h1 className="text-white font-bold">SUMMARY</h1>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
